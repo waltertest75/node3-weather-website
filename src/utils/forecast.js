dbcf08d36b8e8203b16d8eccc1ddaf905344d4a3
Walter Hedrick <walterhedrick@Walters-MacBook-Pro.local>
2020-07-16 15:23:06 -0500

    add humidity and rain

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (long,lat,callback) => {
 
  //   'http://api.weatherstack.com/current?access_key=b4ae7d3d50e9440caafac97386f659cb&query=37.8267,-122.4233&units=f'
@@ -16,7 +18,7 @@ const forecast = (long,lat,callback) => {
             callback('Unable to find location. Try another search')
         } else {
           //  callback(undefined, response.body.current.weather_descriptions[0] + " It is currently "+ response.body.current.temperature + " and it feels like "+ response.body.current.feelslike)
-            callback(undefined, body.current.weather_descriptions[0] + " It is currently "+ body.current.temperature + " and it feels like "+ body.current.feelslike)
+            callback(undefined, body.current.weather_descriptions[0] + " It is currently "+ body.current.temperature + " and it feels like "+ body.current.feelslike + ". The humidity is " + body.current.humidity + "% and there has been " + body.current.precip + " inches of rain")
     }
 })
 }
